55cc7b90ab606dff3f6ccdbacb1bcb89712a4359
Hemant Mandge <hemant.mandge@gmail.com>
2020-12-31 21:36:19 +0530

    Refactoring of package structure

--- src/main/java/com/annotation/factory/autoconfigure/mask/config/MaskSensitiveDataObjectMapperConfig.java
+++ src/main/java/com/annotation/factory/autoconfigure/mask/config/MaskSensitiveDataObjectMapperConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
-public class MaskObjectMapperConfig {
+public class MaskSensitiveDataObjectMapperConfig {
 
     @Primary
     @Autowired
